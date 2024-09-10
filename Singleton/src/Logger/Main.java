c75f19962229bad795a72a4276b1c9142a801880
Ilmarielomaa <ilmari.elomaa03@gmail.com>
2024-09-10 18:35:16 +0300

    assignment 5 singleton done

---
+++ Singleton/src/Logger/Main.java
@@ -0,0 +1,12 @@
+package Logger;
+
+public class Main {
+    public static void main(String[] args) {
+        Logger logger = Logger.getInstance();
+        logger.setFileName("testi.txt"); // Change file name
+        logger.write("Simulation started");
+        logger.write("Processing data...");
+        logger.write("Simulation finished");
+        logger.close(); // Remember to close the logger
+    }
+}
